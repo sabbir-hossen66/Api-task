b70a835d261709f1ad773678d0b09fa12c2f36dd
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-08-29 16:56:46 +0600

    done

--- src/Chart/Blog/Blog.js
+++ src/Chart/Blog/Blog.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const Blog = () => {
+
+
     return (
+
         <div>
-            <h2 className=' text-xl text-center' >How can you use 10M+ data in your DB? Please create a sample DB with dummy data. Along with that please write your system design Approach</h2>
-            <p className='text-blue-600'>Ans: </p>
+
+            <div>
+                <h2>How can you use 10M+ data in your DB? Please create a sample DB with dummy data. Along with that please write your system design Approach</h2><br />
+                <p className='text-blue-600 text-xl'>Ans: Databases are used for storing, maintaining and accessing any sort of data. They collect information on people, places or things. That information is gathered in one place so that it can be observed and analyzed. Databases can be thought of as an organized collection of information.</p>
+
+                see few dummy data: "api.json"
+
+            </div>
         </div>
     );
 };
